38266e92fe73a8c1c5a41a6217cf153ba8b256af
BizCub <bizcubmine@gmail.com>
2025-06-26 10:06:48 +0200

    rename methods

--- src/main/java/com/bizcub/bedrockHotbar/mixin/InGameHudOffsetMixin.java
+++ src/main/java/com/bizcub/bedrockHotbar/mixin/InGameHudOffsetMixin.java
@@ -12,67 +12,67 @@ import org.spongepowered.asm.mixin.injection.ModifyConstant;
 public class InGameHudOffsetMixin {
 
     @ModifyConstant(method = "renderHotbar", constant = @Constant(intValue = 23, ordinal = 0))
-    public int height(int value) {
+    public int resizeSelection(int value) {
         return 24;
     }
 
     @ModifyArg(method = "renderHotbar", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawGuiTexture(Ljava/util/function/Function;Lnet/minecraft/util/Identifier;IIII)V"), index = 3)
-    public int offset1(int value) {
+    public int offsetHotbar(int value) {
         return Offset.operation(value);
     }
 
     @ModifyArg(method = "renderHotbar", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud;renderHotbarItem(Lnet/minecraft/client/gui/DrawContext;IILnet/minecraft/client/render/RenderTickCounter;Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/item/ItemStack;I)V"), index = 2)
-    public int offset2(int value) {
+    public int offsetItem(int value) {
         return Offset.operation(value);
     }
 
     @ModifyArg(method = "renderStatusBars", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud;renderHealthBar(Lnet/minecraft/client/gui/DrawContext;Lnet/minecraft/entity/player/PlayerEntity;IIIIFIIIZ)V"), index = 3)
-    public int offset3(int value) {
+    public int offsetHealth(int value) {
         return Offset.operation(value);
     }
 
     @ModifyArg(method = "renderStatusBars", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud;renderAirBubbles(Lnet/minecraft/client/gui/DrawContext;Lnet/minecraft/entity/player/PlayerEntity;III)V"), index = 3)
-    public int offset4(int value) {
+    public int offsetAir(int value) {
         return Offset.operation(value);
     }
 
     @ModifyArg(method = "renderStatusBars", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud;renderFood(Lnet/minecraft/client/gui/DrawContext;Lnet/minecraft/entity/player/PlayerEntity;II)V"), index = 2)
-    public int offset5(int value) {
+    public int offsetFood(int value) {
         return Offset.operation(value);
     }
 
     @ModifyArg(method = "renderStatusBars", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud;renderArmor(Lnet/minecraft/client/gui/DrawContext;Lnet/minecraft/entity/player/PlayerEntity;IIII)V"), index = 2)
-    public int offset6(int value) {
-        return Offset.operation(value);
-    }
-
-    @ModifyArg(method = "renderMountHealth", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawGuiTexture(Ljava/util/function/Function;Lnet/minecraft/util/Identifier;IIII)V"), index = 3)
-    public int offset7(int value) {
+    public int offsetArmor(int value) {
         return Offset.operation(value);
     }
 
     @ModifyArg(method = "renderExperienceBar", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawGuiTexture(Ljava/util/function/Function;Lnet/minecraft/util/Identifier;IIII)V"), index = 3)
-    public int offset8(int value) {
+    public int offsetExperienceBarBackground(int value) {
         return Offset.operation(value);
     }
 
     @ModifyArg(method = "renderExperienceBar", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawGuiTexture(Ljava/util/function/Function;Lnet/minecraft/util/Identifier;IIIIIIII)V"), index = 7)
-    public int offset9(int value) {
+    public int offsetExperienceBarProgress(int value) {
         return Offset.operation(value);
     }
 
     @ModifyArg(method = "renderExperienceLevel", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawText(Lnet/minecraft/client/font/TextRenderer;Ljava/lang/String;IIIZ)I"), index = 3)
-    public int offset10(int value) {
+    public int offsetExperienceLevel(int value) {
+        return Offset.operation(value);
+    }
+
+    @ModifyArg(method = "renderMountHealth", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawGuiTexture(Ljava/util/function/Function;Lnet/minecraft/util/Identifier;IIII)V"), index = 3)
+    public int offsetMountHealth(int value) {
         return Offset.operation(value);
     }
 
     @ModifyArg(method = "renderMountJumpBar", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawGuiTexture(Ljava/util/function/Function;Lnet/minecraft/util/Identifier;IIII)V"), index = 3)
-    public int offset11(int value) {
+    public int offsetMountJumpBarBackground(int value) {
         return Offset.operation(value);
     }
 
     @ModifyArg(method = "renderMountJumpBar", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawGuiTexture(Ljava/util/function/Function;Lnet/minecraft/util/Identifier;IIIIIIII)V"), index = 7)
-    public int offset12(int value) {
+    public int offsetMountJumpBarProgress(int value) {
         return Offset.operation(value);
     }
 

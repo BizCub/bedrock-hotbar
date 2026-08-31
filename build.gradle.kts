d03a11dfb236b08e136c7e80d52ba4581ce98fe0
BizCub <bizcubmine@gmail.com>
2026-08-31 14:12:12 +0200

    Bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,7 +47,7 @@ multiloader {
         dependency = "maven.modrinth:smooth-scroll:${getDep("smooth-scroll", true)}"
     )
     addDependency(
-        dependency = getSimpleConfigLibDep("1.1"),
+        dependency = getSimpleConfigLibDep(),
         isPublishDepEnabled = true
     )
     val isClothConfigAvailable = !isForge || scp <= "1.21.3"

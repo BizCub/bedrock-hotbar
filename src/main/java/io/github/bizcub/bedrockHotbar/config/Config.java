e2c35f0a4345f8f2be5286e4ad3339115e7a6d7e
BizCub <bizcubmine@gmail.com>
2026-09-13 19:46:21 +0200

    Change the method for invoking the configuration screen when using Simple Config Lib Add the scl suffix to the config created by Simple Config Lib

--- src/main/java/io/github/bizcub/bedrockHotbar/config/Config.java
+++ src/main/java/io/github/bizcub/bedrockHotbar/config/Config.java
@@ -1,20 +1,14 @@
 package io.github.bizcub.bedrockHotbar.config;
 
 import io.github.bizcub.bedrockHotbar.Main;
+import io.github.bizcub.simpleConfigLib.autoconfig.ConfigProvider;
 
 public interface Config {
     static Config get() {
-        return Holder.INSTANCE;
+        return ConfigProvider.get(Config.class);
     }
-
-    static void set(final Config config) {
-        if (config != null) {
-            Holder.INSTANCE = config;
-        }
-    }
-
-    class Holder {
-        private static Config INSTANCE = new Config() { };
+    static void set(Config instance) {
+        ConfigProvider.set(Config.class, instance);
     }
 
     default int offset() {
